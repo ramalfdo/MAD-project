e64eea8a274c21cf38c8ee9a464bc86a754e6174
Duvindu Nishad Karunasingha <Duvidunishad11@gmail.com>
2022-04-13 17:32:41 +0530

    fIXED ALL ERROR WITH THE LOADING SCREEN

---
+++ app/src/main/java/com/example/onlineshop/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.onlineshop
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME:Long=3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed( {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+
+    }
+
+}
