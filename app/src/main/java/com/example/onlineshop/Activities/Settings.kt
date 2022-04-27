b36aae03c8ae6701421bfa4cc89994ab9a1b3922
Duvindu Nishad Karunasingha <Duvidunishad11@gmail.com>
2022-04-28 00:38:57 +0530

    build the dash board icon and update strings/colours/dimens

--- app/src/main/java/com/example/onlineshop/Activities/Settings.kt
+++ app/src/main/java/com/example/onlineshop/Activities/Settings.kt
@@ -3,10 +3,20 @@ package com.example.onlineshop.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.onlineshop.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
-class Settings : AppCompatActivity() {
+class Settings : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+    }
+    private fun setupActionbar(){
+        setSupportActionBar(toolbar_settings_activity)
+        val actionBar=supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_back_24dp)
+        }
+        toolbar_settings_activity.setNavigationOnClickListener{onBackPressed()}
     }
 }
